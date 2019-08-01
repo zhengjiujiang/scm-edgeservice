3dfd6b48716e2ec7f96417253fc435b173e21c76
郑玖江 <zhengjj@kundeyt.com>
2019-08-01 11:15:03 +0800

    增加token转发

--- src/main/java/com/huawei/servicecomb/filter/ApiDispatcher.java
+++ src/main/java/com/huawei/servicecomb/filter/ApiDispatcher.java
@@ -9,11 +9,20 @@ import io.vertx.ext.web.Cookie;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.CookieHandler;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * ApiDispatcher
  */
 public class ApiDispatcher extends AbstractEdgeDispatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiDispatcher.class);
+
     @Override
     public int getOrder() {
         return 10002;
@@ -34,6 +43,8 @@ public class ApiDispatcher extends AbstractEdgeDispatcher {
         String microserviceName = pathParams.get("param0");
         String path = "/" + pathParams.get("param1");
 
+        context.response().putHeader("token",context.request().getHeader("token"));
+
         EdgeInvocation invoker = new EdgeInvocation() {
             /**
              * @throws Exception
@@ -41,8 +52,10 @@ public class ApiDispatcher extends AbstractEdgeDispatcher {
             // Authentication. Notice: adding context must after setContext or will override by network
             protected void setContext() throws Exception {
                 super.setContext();
+                LOGGER.info("context",context);
                 // get session id from header and cookie for debug reasons
                 String token = context.request().getHeader("token");
+                LOGGER.info("token",token);
                 if (token != null) {
                     this.invocation.addContext("token", token);
                 } else {
